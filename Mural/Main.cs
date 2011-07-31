8446c4cb8fac2229ed1772ccb9bae7fdce3286cb
Jason Wodicka <jason@wodicka.net>
2011-07-30 20:58:58 -0700

    Added some TODOs to Main regarding the whole system.

--- Mural/Main.cs
+++ Mural/Main.cs
@@ -11,6 +11,12 @@ namespace Mural
 			// Each module should be unaware of the others. This is the only module that needs to know the lay of the
 			// entire system.
 			
+			// TODO: Replace the logic here that connects components with an IoC system.
+			// Probably use Ninject for this purpose: http://ninject.org/
+			
+			// TODO: Figure out how Mural actually becomes aware of the IP addresses it serves,
+			// and the ports it should listen on. Perhaps the standard .NET .config XML files?
+			
 			// This whole next bit is hackish: It gets an IP address (probably!) that _should_ work for this machine.
 			// No promises are made that it does or it will. It is certain to fail miserably on machines that host
 			//  multiple domains, or have multiple IP addresses, or are behind load balancers, or are interesting in any
@@ -29,11 +35,15 @@ namespace Mural
 			IPAddress ipAddress = ipHostInfo.AddressList[0];
 			int port = 8888;
 			
+			// TODO: Let's get a decent logger in here, shall we?
 			Console.WriteLine("Autodetected net configuration: {0} ({1})", localHostName, ipAddress.ToString());
 			
 			ILineConsumer defaultParser = new LoginParser(); //new RedirectingParser();
 			
 			TelnetListener telnetListener = new TelnetListener(defaultParser, ipAddress, port);
+			// TODO: What does it look like to end the program politely? 
+			// Who handles that, and how do we terminate the listener loops?
+			// Do we support connection-draining?
 			telnetListener.StartListenerLoop(); // This method doesn't return under normal circumstances.
 			
 			Console.WriteLine("Reached the end of the program.");
